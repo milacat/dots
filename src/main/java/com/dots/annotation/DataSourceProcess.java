9f8885df939a2ed5ad322643c72502a426cd242d
milacat <xingmei_v1@163.com>
2019-09-17 16:57:43 +0800

    通过aop使用注解方式切换

--- src/main/java/com/dots/annotation/DataSourceProcess.java
+++ src/main/java/com/dots/annotation/DataSourceProcess.java
@@ -3,27 +3,53 @@ package com.dots.annotation;
 
 import com.dots.util.DynamicDataSource;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.reflect.MethodSignature;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
 
 import java.lang.reflect.Method;
 
+
+@Aspect // for aop
+@Component // for auto scan
 public class DataSourceProcess {
-public void intercept(JoinPoint point) throws NoSuchMethodException {
-    Class<?> target = point.getTarget().getClass();
-    MethodSignature signature = (MethodSignature) point.getSignature();
-    resolveDataSource(target, signature.getMethod());
-   }
-    private void resolveDataSource(Class<?> clazz, Method method) throws NoSuchMethodException {
-     Class<?>[] types = method.getParameterTypes();
-     if(clazz.isAnnotationPresent(DataSource.class)){
-         DataSource source = clazz.getAnnotation(DataSource.class);
-         DynamicDataSource.setDataSource(source.value());
-     }
-        // 方法注解可以覆盖类型注解
-        Method m = clazz.getMethod(method.getName(), types);
-        if (m != null && m.isAnnotationPresent(DataSource.class)) {
-            DataSource source = m.getAnnotation(DataSource.class);
-            DynamicDataSource.setDataSource(source.value());
-        }
+    @Pointcut("execution(public * com.dots.service.UserService.*(..))")
+    public void pointCut() {
+    };
+
+    @Before("pointCut()")
+    public void beforeInvoke(JoinPoint jp) throws ClassNotFoundException {
+       String methodName = jp.getSignature().getName();
+       String clazzName = jp.getTarget().getClass().getName();
+       Class targetClazz = Class.forName(clazzName);
+       Method[] methods = targetClazz.getMethods();
+       for(Method method : methods){
+           if(method.getName().equals(methodName)){
+               if(method.isAnnotationPresent(DataSource.class)){
+                   DataSource dataSource = method.getAnnotation(DataSource.class);
+                   DynamicDataSource.setDataSource(dataSource.value());
+               }
+               break;
+           }
+       }
+    }
+
+    //切换回默认数据库
+    @After("pointCut()")
+    public void afterInvoke(JoinPoint jp) throws ClassNotFoundException {
+        String clazzName = jp.getTarget().getClass().getName();
+        Class targetClazz = Class.forName(clazzName);
+        Method[] methods = targetClazz.getMethods();
+        String methodName = jp.getSignature().getName();
+        for(Method method : methods){
+            if(methodName.equals(method.getName())) {
+                if (method.isAnnotationPresent(DataSource.class)) {
+                    DynamicDataSource.setDataSource("dataSource1");
+                }
+                break;
+            }
+         }
     }
 }
--- src/main/java/com/dots/service/UserService.java
+++ src/main/java/com/dots/service/UserService.java
@@ -4,7 +4,6 @@ import com.dots.annotation.DataSource;
 import com.dots.domain.City;
 import com.dots.domain.Staff;
 import com.dots.dao.UserMapper;
-import com.dots.util.DynamicDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,16 +14,17 @@ public class UserService {
     private UserMapper userMapper;
 
   //  @DataSource("dataSource1")
+  //  dataSource1为默认数据库，无需指定，因为在使用完dataSource2后会切换成dataSource1数据源
     public City getCity(){
         return userMapper.selectByName("Abakan");
     }
 
-  //  @DataSource("dataSource2")
+    @DataSource("dataSource2")
     public Staff getStaff(){
-        //切换数据库
-        DynamicDataSource.setDataSource("dataSource2");
+        //未使用DataSource注解时，手动切换数据库
+//        DynamicDataSource.setDataSource("dataSource2");
          Staff staff = userMapper.selectById(1);
-        DynamicDataSource.setDataSource("dataSource1");
+//        DynamicDataSource.setDataSource("dataSource1");
         return staff;
     }
 
